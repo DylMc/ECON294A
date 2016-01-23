1cad1b858b4834fd11eb5db4546e375bb02567fa
DylMc <dmccombs@ucsc.edu>
2016-01-22 21:03:50 -0800

    Assignment 2 update

--- ECON 294A Assignments/DylanMcCombsAssignment2.R
+++ ECON 294A Assignments/DylanMcCombsAssignment2.R
@@ -43,9 +43,9 @@ NHIS_2007 <- read.table("https://github.com/EconomiCurtis/econ294_2015/raw/maste
 View(NHIS_2007)
 
 DylanMcCombsAssignment2$s2a <- nrow(NHIS_2007)
-DylanMcCombsAssignment2$s2b <- ncol(NHIS_2007)
+DylanMcCombsAssignment2$s2b <- ncol(NHIS_2007) 
 DylanMcCombsAssignment2$s2c <- names(NHIS_2007)
-DylanMcCombsAssignment2$s2d <- mean(NHIS_2007$weight)
+DylanMcCombsAssignment2$s2d <- mean(NHIS_2007$weight) # mean weight
 DylanMcCombsAssignment2$s2e <- median(NHIS_2007$weight)
 
 hist(NHIS_2007$weight)
@@ -59,17 +59,22 @@ View(NHIS_2007)
 hist(NHIS_2007$weightnew) # distribution looks better now
 table(NHIS_2007$weightnew)
 
+# adjusted mean and median
 DylanMcCombsAssignment2$s2f <- mean(NHIS_2007$weightnew, na.rm=TRUE)
 DylanMcCombsAssignment2$s2g <- median(NHIS_2007$weightnew, na.rm=TRUE)
 
 # subsetting the data separately for men and women:
 NHIS_2007women <- subset(NHIS_2007, SEX==2)
 NHIS_2007men <- subset(NHIS_2007, SEX==1)
+
+# weight of women and men adjusted
 DylanMcCombsAssignment2$s2h <- summary(NHIS_2007women$weightnew)
 DylanMcCombsAssignment2$s2i <- summary(NHIS_2007men$weightnew)
 
 ############################################
 # Question 3
+
+# changing letters to factors to extract index numbers as values
 vec <- c(letters,LETTERS)
 vec
 vec_factors <- as.factor(vec)
@@ -86,7 +91,7 @@ DylanMcCombsAssignment2$s3a <- even_vec_index
 DylanMcCombsAssignment2$s3b <- 
   paste(vec[c(30,25,12)], collapse="") # collapses the quotation marks
 
-
+# messing with the array function
 arr <- array( c(letters,LETTERS), dim = c(3,3,3))
 arr
 DylanMcCombsAssignment2$s3c <- arr[1:3,1,2]
